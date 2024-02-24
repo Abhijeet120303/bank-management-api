adfb7ee4b6e82fd628c7daa5737acd8805ab7671
Abhijeet Nirphal <nirphalabhijeet72@gmail.com>
2024-02-24 13:15:25 +0530

    feat(bank): Implement bank management api
              * Implement online bank mangement features
    	  * Implement customer and admin module

---
+++ src/main/java/org/dnyanyog/repo/GetTotalBankAccountRepository.java
@@ -0,0 +1,14 @@
+package org.dnyanyog.repo;
+
+import org.dnyanyog.entity.Account;
+import org.dnyanyog.entity.TotalAccounts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+@Component
+@Repository
+public interface GetTotalBankAccountRepository extends JpaRepository<Account,Long> {
+	
+	
+}
